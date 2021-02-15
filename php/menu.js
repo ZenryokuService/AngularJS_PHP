35db71bb52bc9623a0ce838eda6a92d2d4e39401
余湖 巧 <tkm-yogo@yoshida-g.jp>
2021-02-15 11:20:29 +0900

    とりあえずは、エラーが消えた、モーダル画面が閉じてしまう、減少にぶつかる。

--- php/menu.js
+++ php/menu.js
@@ -1,23 +1,14 @@
+//var app = angular.module('ui.bootstrap.demo', ['ngRoute', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 app.controller('MenuCtrl', function ($scope, $controller) {
 
-	angular.extend(this, $controller('ModalInstanceCtrl', {$scope: $scope}));
+	//angular.extend(this, $controller('ModalInstanceCtrl', {$scope: $scope}));
     document.getElementById("menuContent").innerHTML = $scope.pc.content;
 	//loadJSON($http, "_" + LANG, mc);
 });
 
-function loadJSON($http, sufix, mc) {
-	  var reqParam = {};
-	  reqParam.method = "JSONP";
-	  reqParam.url = "statements/index" + sufix + ".json";
+function createRequest(pc) {
+	return {"lang": LANG,
+			"pc": pc
+	};
+}
 
-	  try {
-	    $http.get(reqParam.url).then(function(res) {
-	    	setData(mc, res.data);
-	    }, function(res) {
-	      console.log(res);
-	    });
-	  } catch(e) {
-	    console.log(e.message);
-	    alert(mc.exception);
-	  }
-	}
--- php/script.js
+++ php/script.js
@@ -4,7 +4,11 @@ var LANG;
 // Code goes here
 var app = angular.module('ui.bootstrap.demo', ['ngRoute', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
+// ルーター設定
 app.config(function($routeProvider) {
+	    // デフォルトは日本語
+	    LANG = "ja";
+
 	$routeProvider
 		.when("/", {
 			templateUrl: "views/login.htm",
@@ -15,74 +19,67 @@ app.config(function($routeProvider) {
 		});
 });
 
-app.controller('ModalDemoCtrl', function ($scope, $http, $uibModal, $log) {
-  //$cacheFactory('cacheId').destroy();
-  var pc = this;
-  pc.data = navigator.appName;
+app.controller('ModalDemoCtrl', function ($scope, $http, $uibModal, $log)  {
+     var pc = this;
+    pc.data = navigator.appName;
 
-  // デフォルトは日本語
-  LANG = "ja";
-  var sufix = "_ja";
-  loadJSON($http, sufix, pc);
+    var sufix = "_ja";
+    loadJSON($http, sufix, pc);
 
-  var size = 1;
-  // モーダル画面を開く
-  pc.open = function (size) {
-    var modalInstance = $uibModal.open({
-      animation: true,
-      ariaLabelledBy: 'modal-title',
-      ariaDescribedBy: 'modal-body',
-      templateUrl: './views/myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      controllerAs: 'pc',
-      size: size,
-      resolve: {
-        data: function () {
-          pc.change(LANG);
-          return pc.data;
-        }
-      }
-    });
+    var size = 1;
+    // モーダル画面を開く
+    pc.open = function (size) {
+  	    var modalInstance = $uibModal.open({
+  	      animation: true,
+  	      ariaLabelledBy: 'modal-title',
+  	      ariaDescribedBy: 'modal-body',
+  	      templateUrl: './views/myModalContent.html',
+  	      controller: 'ModalInstanceCtrl',
+  	      controllerAs: 'pc',
+  	      size: size,
+  	      resolve: {
+  	        data: function () {
+  	          pc.change(LANG);
+  	          return pc.data;
+  	        }
+  	    }
+  	  });
+  	  modalInstance.result.then(function () {
+	    if (LANG == 'ja') {
+	        loadJSON($http, "_ja", pc);
+	    } else {
+	        loadJSON($http, "_en", pc);
+	    }
+	  });
+    }
 
-    modalInstance.result.then(function () {
-        if (LANG == 'ja') {
+    // 日本語と英語を切り替える
+    pc.change = function(lang) {
+        if (lang == 'ja') {
+            LANG = 'ja';
             loadJSON($http, "_ja", pc);
         } else {
+            LANG = 'en';
             loadJSON($http, "_en", pc);
         }
-    });
-  };
-
-  // 日本語と英語を切り替える
-  pc.change = function(lang) {
-    if (lang == 'ja') {
-      LANG = 'ja';
-      loadJSON($http, "_ja", pc);
-    } else {
-      LANG = 'en';
-      loadJSON($http, "_en", pc);
     }
-  };
-
-
-
-});// end of ModalDemoCtrl
+});
 
 function loadJSON($http, sufix, pc) {
-  var reqParam = {};
-  reqParam.method = "JSONP";
-  reqParam.url = "statements/index" + sufix + ".json";
+	var reqParam = {};
+	reqParam.method = "JSONP";
+	reqParam.url = "statements/index" + sufix + ".json";
 
-  try {
-    $http.get(reqParam.url).then(function(res) {
-    	setData(pc, res.data);
-    }, function(res) {
-      console.log(res);
-    });
-  } catch(e) {
-    console.log(e.message);
-    alert(pc.exception);
-  }
+	try {
+	  $http.get(reqParam.url).then(function(res) {
+	  	setData(pc, res.data);
+	  }, function(res) {
+	    console.log(res);
+	  });
+	} catch(e) {
+	  console.log(e.message);
+	  alert(pc.exception);
+	}
 }
 
 /*
@@ -97,13 +94,10 @@ function setData(pc, data) {
     pc.inputUser = data.inputUser;
     pc.inputPassword = data.inputPassword;
     pc.mode = data.fravorMode;
-
-
 }
 
-
 /* モーダル画面の初期処理 */
-angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($scope, $http, $uibModal, $uibModalInstance , data) {
+app.controller('ModalInstanceCtrl', function ($scope, $http, $uibModal, $uibModalInstance , data) {
   var pc = this;
 
   if (LANG == 'ja') {
@@ -121,49 +115,47 @@ angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($s
 		  alert(pc.inputPassword);
 		  return;
 	  }
-
-    //{Log.phpへ、POSTリクエストを送信}
-    $http.post("./module/Login.php", createRequest(pc)).then(function(response) {
-      setData(pc, response.data);
-      if (response.data.isMessage == true) {
-    	  // エラー時の処理
-    	  alert(response.data.mes);
-    	  return;
-      }
-      if (response.data.isMessage == false) {
-    	  $scope.pc.content = response.data.content;
-          location.href = '#!menu';
-    	  //openMenu($uibModal, pc);
-      }
-      //document.getElementById("contentMain").innerHTML = response.data.content;
-    });
+  }
+  //{Log.phpへ、POSTリクエストを送信}
+  $http.post("./module/Login.php", createRequest(pc)).then( function(response) {
+    setData(pc, response.data);
+    if (response.data.isMessage == true) {
+  	  // エラー時の処理
+  	  alert(response.data.mes);
+  	  return;
+    }
+    if (response.data.isMessage == false) {
+  	  $scope.pc.content = response.data.content;
+        location.href = '#!menu';
+  	  //openMenu($uibModal, pc);
+    }
+    //document.getElementById("contentMain").innerHTML = response.data.content;
     $uibModalInstance.close();
-  };
+  });
 
   pc.cancel = function () {
-    //{...}
     $uibModalInstance.dismiss('cancel');
   };
-
+});
 
 //メニューモーダルを開く
-  function openMenu($uibModal, pc) {
-	  var size = 1;
-        var menuModalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-  	      ariaDescribedBy: 'modal-body',
-  	      templateUrl: './views/menu.html',
-  	      controller: 'MenuCtrl',
-  	      controllerAs: 'mc',
-  	      size: size,
-  	      resolve: {
-  	        data: function () {
-  	          //pc.change(LANG);
-  	          return pc.data;
-  	        }
-  	      }
-        });
+ function openMenu($uibModal, pc) {
+	var size = 1;
+	var menuModalInstance = $uibModal.open({
+	    animation: true,
+	    ariaLabelledBy: 'modal-title',
+	    ariaDescribedBy: 'modal-body',
+	    templateUrl: './views/menu.html',
+	    controller: 'MenuCtrl',
+	    controllerAs: 'mc',
+	    size: size,
+	    resolve: {
+	      data: function () {
+	        //pc.change(LANG);
+	        return pc.data;
+	      }
+	    }
+	  });
 
     menuModalInstance.result.then(function () {
         if (LANG == 'ja') {
@@ -173,12 +165,9 @@ angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($s
           }
         //openMenu(pc
     });
-  }
 
-}); // Menu Modal
+}; // Menu Modal
 
 function createRequest(pc) {
-	return {"lang": LANG,
-			"pc": pc
-			};
+	return {"lang": LANG, "pc": pc};
 }
